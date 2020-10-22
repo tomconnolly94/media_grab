2cf1fb246bd649b41036603df70ee42a0f3d7527
tom <tom.connolly@protonmail.com>
2020-10-22 20:59:23 +0200

    refactored logic from Main.py into new LogicController.py

--- Main.py
+++ Main.py
@@ -8,64 +8,24 @@ import logging
 
 # internal dependencies
 from interfaces import TPBInterface, MediaFileInterface
-from controllers import BittorrentController, DataOrganisationController, TorrentFilterController, LoggingController, NewTorrentController
+from controllers import LoggingController, LogicController
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-makeEpisodeQueries = False
-makeSeasonQueries = True
-
 #intitialise logging module
 LoggingController.initLogging()
-
-def findMediaInfoRecord(mediaInfoRecords, mediaInfoName):
-	for record in mediaInfoRecords:
-		if record["name"] == mediaInfoName:
-			return record
 
 
 def main():
 
 	logging.info("Media grab app started.")
 
+	# catch all exceptions so they are always reported
 	try:
 		TPBInterface.init()
 		mediaInfoRecords = MediaFileInterface.loadMediaFile() # information about the wanted media
-		mediaSearchQueries = DataOrganisationController.generateSeasonQueries(mediaInfoRecords)
-
-		for mediaInfoName, queries in mediaSearchQueries.items():
-
-			# make query for the mediaInfoRecord, if none are found, try the next query format
-			for query in queries:
-				torrentQuery = TPBInterface.query('taskmaster')
-
-				if not torrentQuery:
-					continue
-				
-				torrentInfo = []
-
-				for torrent in torrentQuery:
-					torrentInfo.append(torrent)
-				break
-
-
-			#filter torrentInfo by applying regex to torrent titles
-			mediaInfoRecord = findMediaInfoRecord(mediaInfoRecords, mediaInfoName)
-			torrentTitles = [ torrent.title for torrent in torrentInfo ]
-			filteredTorrentTitles = TorrentFilterController.filterSeasonTorrents(torrentTitles, mediaInfoRecord)
-
-			#get list of filtered torrent objects
-			filteredTorrents = [ torrent for torrent in torrentInfo if torrent.title in 
-			filteredTorrentTitles ]
-
-			if filteredTorrents:
-				chosenTorrent = filteredTorrents[0]
-				logging.info(f'torrentInfo: {chosenTorrent}')
-
-				if BittorrentController.initTorrentDownload(chosenTorrent.magnet_link):
-					NewTorrentController.onSuccessfulTorrentAdd(mediaInfoRecord, "latestSeason", chosenTorrent.magnet_link)
-
+		LogicController.runProgramLogic(mediaInfoRecords)
 	
 	except Exception:
 		logging.error("Exception occurred", exc_info=True)
---
+++ controllers/LogicController.py
@@ -0,0 +1,66 @@
+#!/venv/bin/python
+
+# external dependencies
+import logging
+
+# internal dependencies
+from interfaces import TPBInterface, MediaFileInterface
+from controllers import BittorrentController, DataOrganisationController, TorrentFilterController, NewTorrentController
+
+
+def findMediaInfoRecord(mediaInfoRecords, mediaInfoName):
+	for record in mediaInfoRecords:
+		if record["name"] == mediaInfoName:
+			return record
+
+
+def getTorrentRecords(queries):
+
+    torrentRecords = []
+    # make query for the mediaInfoRecord, if none are found, try the next query format
+    for query in queries:
+        torrentQuery = TPBInterface.query(query)
+
+        if not torrentQuery:
+            continue
+        
+        for torrent in torrentQuery:
+            torrentRecords.append(torrent)
+        break
+
+    return torrentRecords
+
+
+def getMediaInfoRecordsWithTorrents(mediaSearchQueries, mediaInfoRecords):
+    mediaInfoRecordsWithTorrents = []
+
+    for mediaInfoName, queries in mediaSearchQueries.items():
+
+        torrentRecords = getTorrentRecords(queries)
+
+        #filter torrentRecords by applying regex to torrent titles
+        mediaInfoRecord = findMediaInfoRecord(mediaInfoRecords, mediaInfoName)
+        torrentTitles = [ torrent.title for torrent in torrentRecords ]
+        filteredTorrentTitles = TorrentFilterController.filterSeasonTorrents(torrentTitles, mediaInfoRecord)
+
+        #get list of filtered torrent objects
+        filteredTorrents = [ torrent for torrent in torrentRecords if torrent.title in filteredTorrentTitles ]
+
+        if filteredTorrents:
+            chosenTorrent = filteredTorrents[0]
+            logging.info(f'torrentInfo: {chosenTorrent}')
+            mediaInfoRecord["magnet_link"] = chosenTorrent.magnet_link
+            mediaInfoRecordsWithTorrents.append(mediaInfoRecord)
+
+    return mediaInfoRecordsWithTorrents
+
+
+def runProgramLogic(mediaInfoRecords):
+    mediaSearchQueries = DataOrganisationController.generateSeasonQueries(mediaInfoRecords)
+
+    mediaInfoRecordsWithTorrents = getMediaInfoRecordsWithTorrents(mediaSearchQueries, mediaInfoRecords)
+
+    for mediaInfoRecord in mediaInfoRecordsWithTorrents:
+        magnet = mediaInfoRecord["magnet_link"]
+        if BittorrentController.initTorrentDownload(magnet):
+            NewTorrentController.onSuccessfulTorrentAdd(mediaInfoRecord, "latestSeason", magnet)
