2f8bd95bb593207aaa0c5f435d68aa2d77ae91db
tom <tom.connolly@protonmail.com>
2020-11-20 14:03:17 +0100

    ADDED: new test file, refactoring for DownloadsInProgressFileInterface.py for conciseness and testability

--- interfaces/DownloadsInProgressFileInterface.py
+++ interfaces/DownloadsInProgressFileInterface.py
@@ -4,17 +4,18 @@
 import json
 import os
 
-def notifyDownloadStarted(mediaType, mediaName):
+file = os.getenv("DOWNLOADS_IN_PROGRESS_FILE")
 
-    file = os.getenv("DOWNLOADS_IN_PROGRESS_FILE")
+def notifyDownloadStarted(mediaType, mediaName):
 
     if not os.path.exists(file):
         createDownloadsInProgressFile(file)
 
-    with open(file, 'rw') as dipFile:
-        media = json.loads(dipFile.read())
+    def operation(media, mediaName=mediaName, mediaType=mediaType):
         media[mediaType].append(mediaName)
+        return media
 
+    updateFile(operation)
 
 
 def createDownloadsInProgressFile(fileLocation):
@@ -29,5 +30,28 @@ def createDownloadsInProgressFile(fileLocation):
     file = open(fileLocation, "x")
     file.write(initialFileContent)
 
-def notifyDownloadFinished():
-    pass
+
+def notifyDownloadFinished(mediaType, mediaName):
+    
+    if not os.path.exists(file):
+        return
+
+    def operation(media, mediaName=mediaName, mediaType=mediaType):
+        media[mediaType].remove(mediaName)
+        return media
+
+    updateFile(operation)
+
+
+def updateFile(operation):
+    with open(file, 'r+') as dipFile:
+        #load file contents to dict
+        fileContent = dipFile.read()
+        media = json.loads(fileContent)
+
+        #add new data to dict
+        operation(media)
+
+        #write new contents of file
+        dipFile.seek(0)
+        dipFile.write(media)
