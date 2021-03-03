04655d483f91b39d290ae3ed488003b3024717a2
tom <tom.connolly@protonmail.com>
2021-03-03 19:37:49 +0100

    ADDED: new install script

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# create python virtual env
+virtualenv venv
+
+# activate virtual env
+source venv/bin/activate
+
+# install pip requirements
+pip install requirements.txt
+
+# create and fill env file
+touch .env
+cat >.env <<EOL
+QBT_URL=http://127.0.0.1:8081
+QBT_USERNAME=admin
+QBT_PASSWORD=qbt_password
+MEDIA_FILE=data/MediaIndex.json
+DOWNLOADS_IN_PROGRESS_FILE=data/DownloadsInProgress.json
+MAIL_USERNAME=app.dev.notifications.tc@gmail.com
+MAIL_PASSWORD=mail_password
+ENVIRONMENT=production
+DUMP_COMPLETE_DIR=./dump_complete_dir
+TV_TARGET_DIR=./tv_target_dir
+... 
+EOL
+
+# create data files
+mkdir data
+
+touch data/MediaIndex.json
+cat >data/MediaIndex.json <<EOL
+{"media": []}
+... 
+EOL
+
+touch data/DownloadsInProgress.json
+cat >data/DownloadsInProgress.json <<EOL
+{"tv-seasons": [], "tv-episodes": []}
+... 
+EOL
+
+echo "Please remember to update your new .env file."
+
+# setup cronjob to run program periodically
