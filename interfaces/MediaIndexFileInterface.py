f7510cc780a53e601301bdd876640ef66301d1f3
tom <tom.connolly@protonmail.com>
2021-03-08 00:35:28 +0100

    CHANGED: extra log comments

--- interfaces/MediaIndexFileInterface.py
+++ interfaces/MediaIndexFileInterface.py
@@ -4,6 +4,7 @@
 import os
 import json
 from interfaces import TheMovieDatabaseInterface
+import logging
 
 
 writeFile = True
@@ -14,13 +15,24 @@ def incrementEpisode(mediaInfoRecords, queryRecord):
 	for mediaRecord in mediaInfoRecords:
 		if mediaRecord["name"] == queryRecord["name"]:
 			maxNumberOfEpisodes = TheMovieDatabaseInterface.getShowEpisodeCount(mediaRecord["name"], mediaRecord["typeSpecificData"]["latestSeason"])
+			prevEpisodeValue = queryRecord["typeSpecificData"]["latestEpisode"]
+			prevSeasonValue = queryRecord["typeSpecificData"]["latestSeason"]
+
 
 			if int(mediaRecord["typeSpecificData"]["latestEpisode"]) + 1 > maxNumberOfEpisodes:
 				# set data to next season first episode
 				mediaRecord["typeSpecificData"]["latestSeason"] = str(int(queryRecord["typeSpecificData"]["latestSeason"]) + 1)
 				mediaRecord["typeSpecificData"]["latestEpisode"] = str(1)
+
+				currentLatestEpisodeValue = queryRecord["typeSpecificData"]["latestEpisode"]
+				currentLatestSeasonValue = queryRecord["typeSpecificData"]["latestSeason"]
+				logging.info(f"Updated latest episode from {prevEpisodeValue} to {currentLatestEpisodeValue}")
+				logging.info(f"Updated latest episode from {prevSeasonValue} to {currentLatestSeasonValue}")
 			else:
 				mediaRecord["typeSpecificData"]["latestEpisode"] = str(int(queryRecord["typeSpecificData"]["latestEpisode"]) + 1)
+
+				currentLatestEpisodeValue = queryRecord["typeSpecificData"]["latestEpisode"]
+				logging.info(f"Updated latest episode from {prevEpisodeValue} to {currentLatestEpisodeValue}")
 
 			return mediaInfoRecords
 	return None
