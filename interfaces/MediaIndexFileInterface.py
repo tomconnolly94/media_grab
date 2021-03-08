4414cb57af57eb168ba992b392686a52c80640ea
tom <tom.connolly@protonmail.com>
2021-03-08 10:25:32 +0100

    CHANGED: increased record update consistency

--- interfaces/MediaIndexFileInterface.py
+++ interfaces/MediaIndexFileInterface.py
@@ -15,8 +15,8 @@ def incrementEpisode(mediaInfoRecords, queryRecord):
 	for mediaRecord in mediaInfoRecords:
 		if mediaRecord["name"] == queryRecord["name"]:
 			maxNumberOfEpisodes = TheMovieDatabaseInterface.getShowEpisodeCount(mediaRecord["name"], mediaRecord["typeSpecificData"]["latestSeason"])
-			prevEpisodeValue = queryRecord["typeSpecificData"]["latestEpisode"]
-			prevSeasonValue = queryRecord["typeSpecificData"]["latestSeason"]
+			prevEpisodeValue = mediaRecord["typeSpecificData"]["latestEpisode"]
+			prevSeasonValue = mediaRecord["typeSpecificData"]["latestSeason"]
 
 
 			if int(mediaRecord["typeSpecificData"]["latestEpisode"]) + 1 > maxNumberOfEpisodes:
@@ -24,14 +24,14 @@ def incrementEpisode(mediaInfoRecords, queryRecord):
 				mediaRecord["typeSpecificData"]["latestSeason"] = str(int(queryRecord["typeSpecificData"]["latestSeason"]) + 1)
 				mediaRecord["typeSpecificData"]["latestEpisode"] = str(1)
 
-				currentLatestEpisodeValue = queryRecord["typeSpecificData"]["latestEpisode"]
-				currentLatestSeasonValue = queryRecord["typeSpecificData"]["latestSeason"]
+				currentLatestEpisodeValue = mediaRecord["typeSpecificData"]["latestEpisode"]
+				currentLatestSeasonValue = mediaRecord["typeSpecificData"]["latestSeason"]
 				logging.info(f"Updated latest episode from {prevEpisodeValue} to {currentLatestEpisodeValue}")
-				logging.info(f"Updated latest episode from {prevSeasonValue} to {currentLatestSeasonValue}")
+				logging.info(f"Updated latest season from {prevSeasonValue} to {currentLatestSeasonValue}")
 			else:
-				mediaRecord["typeSpecificData"]["latestEpisode"] = str(int(queryRecord["typeSpecificData"]["latestEpisode"]) + 1)
+				mediaRecord["typeSpecificData"]["latestEpisode"] = str(int(mediaRecord["typeSpecificData"]["latestEpisode"]) + 1)
 
-				currentLatestEpisodeValue = queryRecord["typeSpecificData"]["latestEpisode"]
+				currentLatestEpisodeValue = mediaRecord["typeSpecificData"]["latestEpisode"]
 				logging.info(f"Updated latest episode from {prevEpisodeValue} to {currentLatestEpisodeValue}")
 
 			return mediaInfoRecords
