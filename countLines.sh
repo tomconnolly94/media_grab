ed6ab7e1f9d8f10ca87cdc61fc5c653e492c17d5
tom <tom.connolly@protonmail.com>
2021-03-24 11:08:54 +0100

    CHANGED: excluded venv-dev too

--- countLines.sh
+++ countLines.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -path ./venv -prune -false -o  -name "*.py" -print0 | xargs -0 wc -l
+find . -type d \( -path ./venv -o -path ./venv-dev \) -prune -false -o  -name "*.py" -print0 | xargs -0 wc -l
