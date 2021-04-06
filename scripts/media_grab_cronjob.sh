8b8b7ee7b01ff86efb22c685e56b77d5d32616eb
tomcon25 <tom.connolly@protonmail.com>
2021-04-06 08:43:10 +0200

    CHANGED: updates to default cronjob file

--- scripts/media_grab_cronjob.sh
+++ scripts/media_grab_cronjob.sh
@@ -2,4 +2,4 @@
 # (c) 2020 media_grab
 #
 # Runs media_grab at 3 hour intervals
-0 */3 * * * root /home/tom/projects/media_grab/venv/bin/python3 /home/tom/projects/media_grab/Main.py
+0 */3 * * * root cd /home/tom/projects/media_grab; venv/bin/python3 Main.py
