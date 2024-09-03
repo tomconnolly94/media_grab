298ad369634486720f3da6f087da1056a2aa25bb
Tom <tom@emal.com>
2024-09-03 22:16:13 +0100

    docker stuff, qbittorrent default config stuff

--- media_grab-container/entrypoint.sh
+++ media_grab-container/entrypoint.sh
@@ -3,6 +3,11 @@
 # export environment variables to a file so that the app can use them later
 printenv > /proj-dir/.env
 
+# create MediaIndex.json if it does not exist
+if [ ! -f /data/MediaIndex.json ]; then
+    cp ./templates/MediaIndex-template.json /data/MediaIndex.json
+fi
+
 # run cron in the background
 cron
 
