2589e08fc5e65e4931e54ef53abc679d5a01cc3b
Tom <tom@emal.com>
2024-09-03 23:08:15 +0100

    fixed qbittorrent default installation sequence

--- qbittorrent-container/entrypoint.sh
+++ qbittorrent-container/entrypoint.sh
@@ -2,52 +2,16 @@
 # using https://github.com/bashup/dotenv
 
 # define conf file
-CONFIG_FILE=/config/qBittorrent/qBittorrent.conf
+defaultConfigFile=./config-template/qBittorrent.template.conf
+targetConfigDir=/config/qBittorrent
+targetConfigFile=$targetConfigDir/qBittorrent.conf
 
-# check if temp dir is provided
-temp_dir_set=false
-
-# inject variables into qBittorrent.conf
-./dotenv.sh -f $CONFIG_FILE set "Session\DefaultSavePath"=$DUMP_COMPLETE_DIR
-./dotenv.sh -f $CONFIG_FILE set "Downloads\SavePath"=$DUMP_COMPLETE_DIR
-./dotenv.sh -f $CONFIG_FILE set "Session\MaxActiveDownloads=3"
-./dotenv.sh -f $CONFIG_FILE set "Session\MaxActiveTorrents=5"
-./dotenv.sh -f $CONFIG_FILE set "Session\MaxActiveUploads=2"
-./dotenv.sh -f $CONFIG_FILE set "Session\QueueingSystemEnabled=true"
-
-# remove
-./dotenv.sh -f $CONFIG_FILE set "WebUI\AuthSubnetWhitelist=172.18.0.1/32"
-./dotenv.sh -f $CONFIG_FILE set "WebUI\AuthSubnetWhitelistEnabled=true"
-
-if [ -n "$DUMP_TEMP_DIR" ] ; then
-    ./dotenv.sh -f $CONFIG_FILE set "Session\TempPath"=$DUMP_TEMP_DIR
-    ./dotenv.sh -f $CONFIG_FILE set "Session\TempPathEnabled"=true
-    ./dotenv.sh -f $CONFIG_FILE set "Downloads\TempPath"=$DUMP_TEMP_DIR
-    ./dotenv.sh -f $CONFIG_FILE set "Downloads\TempPathEnabled"=true
+# install our default conf file if it has not been updated from the boilerplate version
+if [ ! -f $targetConfigFile ]; then
+    echo "Installing default qBittorrent.conf file location=$targetConfigFile"
+    mkdir -p $targetConfigDir
+    cp $defaultConfigFile $targetConfigFile
 fi
-
-if [ -n "$MAIL_NOTIFICATION_ADDRESS" ]; then
-    ./dotenv.sh -f $CONFIG_FILE set "MailNotification\email"=$MAIL_NOTIFICATION_ADDRESS
-fi
-
-if [ -n "$MAIL_USERNAME" ]; then
-    ./dotenv.sh -f $CONFIG_FILE set "MailNotification\sender"=$MAIL_USERNAME
-    ./dotenv.sh -f $CONFIG_FILE set "MailNotification\username"=$MAIL_USERNAME
-fi
-
-if [ -n "$MAIL_PASSWORD" ]; then
-    ./dotenv.sh -f $CONFIG_FILE set "MailNotification\password"=$MAIL_PASSWORD
-fi
-
-CONFIG_DIR_TARGET=/config/qBittorrent
-
-if [[ ! -d $CONFIG_DIR_TARGET ]]
-then
-    mkdir $CONFIG_DIR_TARGET
-fi
-
-
-# cp $CONFIG_FILE /config/qBittorrent/qBittorrent.conf
 
 echo "Starting qbittorrent with new qBittorrent.conf file."
 
